586ac7245524a14e57cb839a462b06d9cd4c7a99
IonutHaita <ionut.haita93@gmail.com>
2024-10-01 05:29:15 +0300

    .

---
+++ Cypher_generator.py
@@ -0,0 +1,34 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '.', ',', '<', '>', '/', '?', ';', ':', '"', '|', '[', '{', ']', '}', '-', '+', '`', '~', '!', '@', '#', '$', '%', '^', '&', '(', ')', '_', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+text = ""
+
+
+
+def encrypt(text):
+    ori_text = input("Type your message to be encrypted: \n")
+    shift_no = int(input("Enter the shift number:\n"))
+
+    for letter in ori_text:
+        index = alphabet.index(letter)
+        print(index)
+        text += alphabet[index + shift_no]
+    return text
+
+def decrypt(text):
+    ori_text = input("Type your message to be decrypted: \n")
+    shift_no = int(input("Enter the shift number:\n"))
+
+    for letter in ori_text:
+        index = alphabet.index(letter)
+        print(index)
+        text += alphabet[index - shift_no]
+    return text
+
+
+decision = input("Please type encrypt to encrypt a message or decrypt to decrypt one:\n")
+if decision == "encrypt":
+    modified_text = encrypt(text)
+elif decision == "decrypt":
+    modified_text = decrypt(text)
+else:
+    print("Invalid input, try again!")
+print(modified_text)
